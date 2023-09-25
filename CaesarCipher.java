0e94f62ed5e0fea74325318fc1672775495b8744
Vikram M <82879308+Vikram-M-hub@users.noreply.github.com>
2023-09-25 19:44:59 +0530

    Update CaesarCipher.java

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,9 +1,8 @@
 
 public class CaesarCipher {
 
-    // 'A'/'a' + (str[i] - 'a'/'A' + offset) % 26
     public static String encode(String enc, int offset) {
-        // offset = offset % 26 + 26;
+
         StringBuilder encoded = new StringBuilder();
         for (char i : enc.toCharArray()) {
             if (Character.isLetter(i)) {
